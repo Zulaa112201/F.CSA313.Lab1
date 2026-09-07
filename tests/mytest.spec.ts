c0d2cbf05e16a890bcbd2f6792c91cc1c6abb8fc
Zulaa112201 <_@.com>
2026-09-07 19:45:41 +0800

    sorog test

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+
+test('1. Амжилттай нэвтрэх тест', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+    
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    
+    await page.getByRole('button', { name: 'Login' }).click();
+    
+    
+    await expect(page.getByText('Products')).toBeVisible();
+
+
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
+
+    test('2. Амжилтгүй нэвтрэх сөрөг тест', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+    
+        await page.getByPlaceholder('Username').fill('standard_user');
+        await page.getByPlaceholder('Password').fill('wrong_password');
+        await page.getByRole('button', { name: 'Login' }).click();
+        
+        
+        await expect(page.getByText('Epic sadface')).toBeVisible();
+    });
+
+
+    test('3. Бараа сагслах үйлдэл', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        
+        
+        await page.getByPlaceholder('Username').fill('standard_user');
+        await page.getByPlaceholder('Password').fill('secret_sauce');
+        await page.getByRole('button', { name: 'Login' }).click();
+        
+        
+        await page.getByRole('button', { name: 'Add to cart' }).first().click();
+        
+
+        await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+
+    
+        await page.getByRole('button', { name: 'Open Menu' }).click();
+        await page.getByRole('link', { name: 'Logout' }).click();
+        
+    
+        await expect(page).toHaveURL('https://www.saucedemo.com/');
+    });
