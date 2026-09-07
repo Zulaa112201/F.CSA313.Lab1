52545b474e7add9fa995250dfc2f68e27f6e4ad8
Zulaa112201 <_@.com>
2026-09-07 20:42:39 +0800

    suuliin zaswar orj duusaw

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,13 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+  /* Base URL to use in actions like `await page.goto('')`. */
+  // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+  /* Collect trace for all tests */
+  trace: 'on',
+  video: 'on',
+},
 
   /* Configure projects for major browsers */
   projects: [
